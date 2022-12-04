1d76a022bebf134a256f68cadcc640c65ac3cb1c
Gyulnara <zadoyan.gyulnara@mail.ru>
2022-12-04 13:31:48 +0400

    cerating login/register and some part in account page

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ $('.sub').click(function () {
   }
 });
 
+
 $('.multiple-items').slick({
   infinite: true,
   slidesToShow: 5,
@@ -200,3 +201,19 @@ centerMode     : true
 }
 
 
+function openAccountDetail(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
